26ca3b09f0eba604cb2a9fa8930b4317c3f80c0c
Minela Ganovic <minelag556@gmail.com>
2023-02-17 17:50:24 +0100

    Change index

--- public/index.php
+++ public/index.php
@@ -31,8 +31,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
-
+require __DIR__.'/vendor/autoload.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 /*
 |--------------------------------------------------------------------------
 | Run The Application
