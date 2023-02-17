9215675f8697e96de755d5d2729e59c0839b7909
Minela Ganovic <minelag556@gmail.com>
2023-02-17 17:51:23 +0100

    Change dir index

--- public/index.php
+++ public/index.php
@@ -44,8 +44,6 @@ $app = require_once __DIR__.'/bootstrap/app.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
