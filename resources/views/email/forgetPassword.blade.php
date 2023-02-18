9ca631c0f45657b1067ab4d6eeee712c280b89c5
Minela Ganovic <minelag556@gmail.com>
2023-02-18 09:14:55 +0100

    I changed something

--- resources/views/email/forgetPassword.blade.php
+++ resources/views/email/forgetPassword.blade.php
@@ -1,4 +1,4 @@
-<h1>Zaboravi Lozinku Email</h1>
+<h1>Promeni Lozinku:</h1>
    
 Ako želite promenite šifu idite na sledeći link :
 <a href="{{ route('reset.password.get', $token) }}">Promena Lozinke</a>
